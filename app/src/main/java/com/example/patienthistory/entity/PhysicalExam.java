964bb091cb6f970da48f26e515bccb95ec26ae3c
Mohamed Samy <mamo.sha3ar@gmail.com>
2019-06-13 19:04:09 +0200

    added physical exam entity and attributes and removed Nullable annotations from social habit

--- app/src/main/java/com/example/patienthistory/entity/PhysicalExam.java
+++ app/src/main/java/com/example/patienthistory/entity/PhysicalExam.java
@@ -1,24 +1,20 @@
 package com.example.patienthistory.entity;
 
-import androidx.annotation.Nullable;
 import androidx.room.Entity;
 import androidx.room.ForeignKey;
 import androidx.room.PrimaryKey;
 
 @Entity(foreignKeys = @ForeignKey(entity = Patient.class,
-        parentColumns = "socialHabitId",
-        childColumns = "patientId",
+        parentColumns = "physicalExamId",
+        childColumns = "company_id",
         onDelete = ForeignKey.CASCADE))
-public class SocialHabit {
+public class PhysicalExam {
 
     @PrimaryKey(autoGenerate = true)
     private int id;
+
     private int patientId;
 
-    private boolean drugs;
-
-    private boolean tobacco;
-
-    private boolean alcohol;
-
+    private String bloodPressure;
+    private String heartRate;
 }
