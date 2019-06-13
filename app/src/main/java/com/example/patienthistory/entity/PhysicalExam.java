87e037a8913e04068337b37ca2e843bf71ca988a
Mohamed Samy <mamo.sha3ar@gmail.com>
2019-06-13 19:30:52 +0200

    added constructor, setters and getters

--- app/src/main/java/com/example/patienthistory/entity/PhysicalExam.java
+++ app/src/main/java/com/example/patienthistory/entity/PhysicalExam.java
@@ -17,4 +17,42 @@ public class PhysicalExam {
 
     private String bloodPressure;
     private String heartRate;
+
+    public PhysicalExam(int patientId, String bloodPressure, String heartRate) {
+        this.patientId = patientId;
+        this.bloodPressure = bloodPressure;
+        this.heartRate = heartRate;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getPatientId() {
+        return patientId;
+    }
+
+    public void setPatientId(int patientId) {
+        this.patientId = patientId;
+    }
+
+    public String getBloodPressure() {
+        return bloodPressure;
+    }
+
+    public void setBloodPressure(String bloodPressure) {
+        this.bloodPressure = bloodPressure;
+    }
+
+    public String getHeartRate() {
+        return heartRate;
+    }
+
+    public void setHeartRate(String heartRate) {
+        this.heartRate = heartRate;
+    }
 }
