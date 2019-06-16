a39f06c7394b244fedfa4670ca689e7bc7fea821
Mohamed Samy <mamo.sha3ar@gmail.com>
2019-06-16 23:03:41 +0200

    added first sign up activity and modified register button to start that activity

--- app/src/main/java/com/example/patienthistory/MainActivity.java
+++ app/src/main/java/com/example/patienthistory/MainActivity.java
@@ -1,46 +1,35 @@
 package com.example.patienthistory;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProviders;
-
+import android.content.Intent;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.view.View;
+import android.widget.Button;
 
-import com.example.patienthistory.room.dao.AllergiesDao;
-import com.example.patienthistory.room.entities.Allergies;
-import com.example.patienthistory.room.entities.Patient;
-import com.example.patienthistory.room.viewmodels.AllergiesViewModel;
-import com.example.patienthistory.room.viewmodels.PatientViewModel;
+import com.example.patienthistory.register_user.SignUpInfoActivity;
 
-import org.w3c.dom.Text;
-
-import java.util.OptionalLong;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    private PatientViewModel patientViewModel;
-    private AllergiesViewModel allergiesViewModel;
-
-    //public AllergiesDao allergiesDao;
-
-    private TextView textView;
-
+    private Button btn_signIn;
+    private Button btn_register;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        patientViewModel = ViewModelProviders.of(this).get(PatientViewModel.class);
-        patientViewModel.insert(new Patient("64 gamet el dowal", "A+", "complicated", 0));
-        //Patient temp = new Patient("63  gamet el dowal", "A+", "single", 0);
-        //temp.setId(1);
-        //patientViewModel.update(temp);
-        Allergies allergies = new Allergies(1, "Lactose");
-        //allergiesDao.insert(allergies);
-        allergiesViewModel = ViewModelProviders.of(this).get(AllergiesViewModel.class);
-        allergiesViewModel.insert(allergies);
+        btn_signIn = findViewById(R.id.btn_sign_in);
+        btn_register = findViewById(R.id.btn_register);
+
+
+        btn_register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SignUpInfoActivity.class);
+                startActivity(intent);
+            }
+        });
 
     }
 }
---
+++ app/src/main/java/com/example/patienthistory/register_user/SignUpInfoActivity.java
@@ -0,0 +1,16 @@
+package com.example.patienthistory.register_user;
+
+import android.os.Bundle;
+
+import com.example.patienthistory.R;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SignUpInfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up_info);
+    }
+}
