89bff8439f1f9287031beafe7e0bf0e9019e7023
Mohamed Samy <mamo.sha3ar@gmail.com>
2019-06-17 20:50:30 +0200

    finished startup xml

---
+++ app/src/main/java/com/example/patienthistory/register_user/PickImageAndConfirmActivity.java
@@ -0,0 +1,16 @@
+package com.example.patienthistory.register_user;
+
+import android.os.Bundle;
+
+import com.example.patienthistory.R;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class PickImageAndConfirmActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pick_image_and_confirm);
+    }
+}
