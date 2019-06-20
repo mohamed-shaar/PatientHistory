0ae236619123916afeb47b6386b8b6c546211e2a
Mohamed Samy <mamo.sha3ar@gmail.com>
2019-06-21 01:51:08 +0200

    implemented Data Fragment and Add Data fragment

--- app/src/main/java/com/example/patienthistory/fragments/DataFragment.java
+++ app/src/main/java/com/example/patienthistory/fragments/DataFragment.java
@@ -4,34 +4,51 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.Button;
 
 import com.example.patienthistory.R;
-import com.example.patienthistory.room.entities.Patient;
-import com.example.patienthistory.room.viewmodels.PatientViewModel;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProviders;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 public class DataFragment extends Fragment {
+
+    private Button btn_patient_data;
+    private Button btn_patient_family_diseases;
+    private Button btn_patient_allergies;
+    private Button btn_patient_dietary_information;
+    private Button btn_patient_remedies;
+    private Button btn_patient_social_habit;
+    private Button btn_patient_physical_exam;
+    private Button btn_patient_surgeries;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.fragment_data, container, false);
-        final TextView textView = view.findViewById(R.id.tv_patient_data);
+        btn_patient_data = view.findViewById(R.id.btn_patient_data);
+        btn_patient_family_diseases = view.findViewById(R.id.btn_patient_family_diseases);
+        btn_patient_allergies = view.findViewById(R.id.btn_patient_allergies);
+        btn_patient_dietary_information = view.findViewById(R.id.btn_patient_dietary_information);
+        btn_patient_remedies = view.findViewById(R.id.btn_patient_remedies);
+        btn_patient_social_habit = view.findViewById(R.id.btn_patient_social_habit);
+        btn_patient_physical_exam = view.findViewById(R.id.btn_patient_physical_exam);
+        btn_patient_surgeries = view.findViewById(R.id.btn_patient_surgeries);
 
-        PatientViewModel patientViewModel = ViewModelProviders.of(this).get(PatientViewModel.class);
-        patientViewModel.getData().observe(this, new Observer<Patient>() {
+        btn_patient_data.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onChanged(Patient patient) {
-                textView.append(String.valueOf(patient.getId()) + "\n");
-                textView.append(String.valueOf(patient.getNumberOfChildren()) + "\n");
-                textView.append(patient.getAddress() + "\n");
-                textView.append(patient.getBloodType());
+            public void onClick(View v) {
+                AddDataFragment fragment = new AddDataFragment();
+                FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.patient_frame_layout, fragment);
+                fragmentTransaction.addToBackStack(null);
+                fragmentTransaction.commit();
+                //getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.fragment_add_data, new AddDataFragment(), null).commit();
             }
         });
 
